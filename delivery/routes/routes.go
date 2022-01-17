ea7df6bd030b20a38ebe52e200ebd766365a9d15
asgard <asgard@Aphrodite.local>
2022-01-17 20:11:12 +0800

    initial sqldriver and routes

---
+++ delivery/routes/routes.go
@@ -0,0 +1,11 @@
+package routes
+
+import (
+	"project-e-commerces/delivery/controllers/carts"
+	"project-e-commerces/delivery/controllers/transactions"
+
+	"github.com/labstack/echo/v4"
+)
+
+func RegisterPath(e *echo.Echo, crCtrl *carts.CartsController, tsCtrl *transactions.TransactionsController) {
+}
