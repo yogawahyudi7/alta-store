bcc3fac1ee36cfe137194af7e9dd7010224e761b
asgard <asgard@Aphrodite.local>
2022-01-17 20:12:13 +0800

    changed main

--- main.go
+++ main.go
@@ -1,8 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"project-e-commerces/configs"
+	"project-e-commerces/delivery/controllers/carts"
+	"project-e-commerces/delivery/controllers/transactions"
+	"project-e-commerces/delivery/routes"
+	cartsRepo "project-e-commerces/repository/carts"
+	transactionsRepo "project-e-commerces/repository/transactions"
+	"project-e-commerces/utils"
+
+	"github.com/labstack/echo/v4"
+)
 
 func main() {
 
-	fmt.Println("hello guys1")
+	config := configs.GetConfig()
+	db := utils.InitDB(config)
+
+	e := echo.New()
+
+	cartsRepo := cartsRepo.NewCartsRepo(db)
+	cartsCtrl := carts.NewCartsControllers(cartsRepo)
+
+	transactionsRepo := transactionsRepo.NewTransactionsRepo(db)
+	transactionsCtrl := transactions.NewTransactionsControllers(transactionsRepo)
+
+	routes.RegisterPath(e, cartsCtrl, transactionsCtrl)
+
+	address := fmt.Sprintf("localhost:%d", config.Port)
+	log.Fatal(e.Start(address))
 }
