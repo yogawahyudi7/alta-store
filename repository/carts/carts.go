5fae48123f1215c134057574063df4ba57f5e07f
asgard <asgard@Aphrodite.local>
2022-01-17 20:10:23 +0800

    initial carts repository

--- repository/carts/carts.go
+++ repository/carts/carts.go
@@ -1,1 +1,21 @@
 package carts
+
+import (
+	"project-e-commerces/entities"
+
+	"gorm.io/gorm"
+)
+
+type CartsRepository struct {
+	db *gorm.DB
+}
+
+func NewCartsRepo(db *gorm.DB) *CartsRepository {
+	return &CartsRepository{db: db}
+}
+
+func (cr *CartsRepository) Gets() ([]entities.Cart, error) {
+	carts := []entities.Cart{}
+	cr.db.Find(&carts)
+	return carts, nil
+}
