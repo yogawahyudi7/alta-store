3306c69a03c9ad2ebd2e5d6fc7e00bca348f1ca1
ellashella24 <ella.shella24@gmail.com>
2022-01-18 20:38:56 +0700

    add products & categorys repository

---
+++ repository/products/interface.go
@@ -0,0 +1,12 @@
+package products
+
+import "project-e-commerces/entities"
+
+type ProductInterface interface {
+	GetAllProduct() ([]entities.Product, error)
+	GetProductByID(id int) (entities.Product, error)
+	CreateProduct(product entities.Product) (entities.Product, error)
+	UpdateProduct(product_id int, product entities.Product) (entities.Product, error)
+	UpdateStockProduct(product_id, qty int) (entities.Product, error)
+	DeleteProduct(product_id int) (entities.Product, error)
+}
