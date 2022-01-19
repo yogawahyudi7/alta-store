c674ba350b22ec5458af7038feb908c6be4ed926
asgard <asgard@Aphrodite.local>
2022-01-19 18:44:35 +0800

    fix bug update qty

--- repository/carts/carts.go
+++ repository/carts/carts.go
@@ -25,9 +25,22 @@ func (cr *CartsRepository) Insert(newCart entities.Cart) (entities.Cart, error) 
 	return newCart, nil
 }
 
-func (cr *CartsRepository) InsertProduct(newProduct entities.Detail_cart) (entities.Detail_cart, error) {
-
+func (cr *CartsRepository) InsertProduct(cartID uint, newProduct entities.Detail_cart) (entities.Detail_cart, error) {
 	cr.db.Save(&newProduct)
-
 	return newProduct, nil
+}
+
+func (cr *CartsRepository) UpdateProduct(cartID, productID uint, qty int) (entities.Detail_cart, error) {
+	detail_cart := entities.Detail_cart{}
+	cr.db.Where("cart_id=? AND product_id=?", cartID, productID).Find(&detail_cart)
+
+	detail_cart.Qty += qty
+	cr.db.Save(&detail_cart)
+	return detail_cart, nil
+}
+
+func (cr *CartsRepository) DeleteProduct(cartID, productID uint) (entities.Detail_cart, error) {
+	carts := entities.Detail_cart{}
+	cr.db.Find(&carts, "cart_id=? AND product_id=?", cartID, productID).Delete(&carts)
+	return carts, nil
 }
--- repository/carts/interface.go
+++ repository/carts/interface.go
@@ -5,4 +5,7 @@ import "project-e-commerces/entities"
 type CartInterface interface {
 	Gets() ([]entities.Cart, error)
 	Insert(newCart entities.Cart) (entities.Cart, error)
+	InsertProduct(cartID uint, newItem entities.Detail_cart) (entities.Detail_cart, error)
+	UpdateProduct(cartID, productID uint, qty int) (entities.Detail_cart, error)
+	DeleteProduct(cartID, productID uint) (entities.Detail_cart, error)
 }
